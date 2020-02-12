a59e8c70ea1d84f45164a35f148368fcca1230bc
oxymonster <giorgizeikidze97@gmail.com>
2020-02-12 21:24:20 +0400

    $$

--- models/economy-model.js
+++ models/economy-model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const economySchema = new mongoose.Schema({
     title: String, 
     description: String,
-    date: Date,
+    date: String,
     file: Object
 }); 
 
